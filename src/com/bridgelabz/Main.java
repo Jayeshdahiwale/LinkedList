d09d5174d8589eb52008362d946fab8293ccbb35
JAYESH <jayjdahivale@gmail.com>
2022-03-20 19:40:49 +0530

    Added

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -9,8 +9,8 @@ public class Main {
 		LL list =new LL();
 		list.addLast(56);
 		list.addLast(70);
-		list.insertBetween(56, 70, 30);
-		list.deleteFirst();
+		list.insertBetween(56, 70, 30);//Inserting 30 between 56 & 70
+		list.deleteLast();
 		list.printList();
 		System.out.println(list.getSize());
 	}
